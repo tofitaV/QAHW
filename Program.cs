5b56add26ed54b5a1a2095074f077de4eb85d286
Vlad <48254481+tofitaV@users.noreply.github.com>
2019-11-08 16:14:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp
+{
+    class Program : IEnumerator, IEnumerable
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                Console.WriteLine(test.Current());
+                Console.WriteLine(test.MoveNext());
+            }
+
+        }
+
+        public static int[] arr = { 1, 2,5555, 3,444, 4, 5, 6, 8 };
+        int Counter;
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public object Current()
+        {
+            return arr[Counter];
+        }
+
+         public bool MoveNext()
+        {
+            if(Counter < arr.Length)
+            {
+                Counter++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            throw new NotImplementedException();
+            
+        }
+
+        public void Reset()
+        {
+            Counter = -1;
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
