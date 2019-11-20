a10f6b1cb71f794e6a94af0980e0aa2646ce812a
tofita <tofita1337@gmail.com>
2019-11-20 16:15:13 +0200

    1

---
+++ WorkWithInfo.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace JsonHW
+{
+    class WorkWithInfo
+    {
+        public string line = "";
+        
+        public static JsonClass ParseJson(string jsonMassive)
+        {
+       
+            JsonClass json = JsonConvert.DeserializeObject<JsonClass>(jsonMassive);
+
+            return json;
+        }
+
+        
+    }
+}
